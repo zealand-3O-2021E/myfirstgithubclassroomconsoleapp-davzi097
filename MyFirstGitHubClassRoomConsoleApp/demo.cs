3e3e7378a3503a4abb5ffbb397b8890621faf16c
David Zikla <davi048b@edu.easj.dk>
2021-09-01 08:24:29 +0200

    demo assigment from 31th August

---
+++ MyFirstGitHubClassRoomConsoleApp/demo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class demo
+    {
+        private int _a;
+        private int _b;
+
+        public demo(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public int A { get => _a; set => _a = value; }
+        public int B { get => _b; set => _b = value; }
+    }
+}
